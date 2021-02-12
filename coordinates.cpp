c3d2da5664006b93b77b881b10645f5988bac736
Baila Zwiebel <zwiebelb@sou.edu>
2021-02-12 14:20:39 -0800

    coordinate functions

--- coordinates.cpp
+++ coordinates.cpp
@@ -16,17 +16,16 @@ double Haversine(Coordinate a, Coordinate b){
     double lon2 = b[1].second;
 
     const double R = 6371e3;
-    const double sig1 = lat1 * M_PI/180;
-    const double sig2 = lat2 * M_PI/180;
-    const double deltasig = (lat2-lat1) * M_PI/180;
+    const double phi1 = lat1 * M_PI/180;
+    const double phi2 = lat2 * M_PI/180;
+    const double deltaphi = (lat2-lat1) * M_PI/180;
     const double deltalamda = (lon2-lon1) * M_PI/180;
 
     //a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
-    const double aH = pow(sin(deltasig/2.0),2.0)+cos(sig1)*cos(sig2)*pow(sin(deltalamda/2),2);
+    const double aH = pow(sin(deltaphi/2.0),2.0)+cos(phi1)*cos(phi2)*pow(sin(deltalamda/2),2);
     //c = 2 ⋅ atan2( √a, √(1−a) )
     const double c = 2 * atan2(sqrt(aH),sqrt(1.0-aH)); 
     const double d = (R * c)/1000;
-    cout << "d "<< d <<endl;
 
     return d;}
 
