790f72ecc9f6651d929714230a1ee8b740207cfc
Baila Zwiebel <zwiebelb@sou.edu>
2021-02-11 17:30:33 -0800

    functions

---
+++ coordinates.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "coordinates.h"
+
+using namespace std;
+
+
+double Haversine(Coordinate a, Coordinate b){return 0.0;}
+
+//target is 42.1858587, -122.6970897
+Coordinate SortByDistanceTo(Coordinate target){
+    Coordinate distance;
+    return distance;}
