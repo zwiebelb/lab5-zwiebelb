ebdabc7f5ce5727d612e4b07e5da82e3ccd9a3e0
Baila Zwiebel <zwiebelb@sou.edu>
2021-02-17 17:38:26 -0800

    main function

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main() {
   SortByDistanceTo(target, coords);
 
   cout << "\nThe closest coordinate to the SOU CS building is: " << fixed
-       << setprecision(6) << coords[0].first << ", " << coords[0].second
+       << setprecision(7) << coords[0].first << ", " << coords[0].second
        << endl;
 
   return 0;
