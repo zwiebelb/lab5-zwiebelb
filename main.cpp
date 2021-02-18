452a5a555063be847db1fc09d5bef1458ba50e93
Baila Zwiebel <zwiebelb@sou.edu>
2021-02-17 17:35:50 -0800

    main function

--- main.cpp
+++ main.cpp
@@ -1,43 +1,33 @@
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include "coordinates.h"
 
 using namespace std;
+
 int main() {
-  string c;
-  Coordinate vect;
 
-  double a;
-  double b;
- //double distance;
-  Coordinate c1(2, std::make_pair(42.1858587, -122.6970897));
-  while (getline(cin, c)) {
-    int length = c.size();
-    for (int i = 0; i < length; ++i) {
-      if (c.at(i) == ',') {
-        a = atof(c.substr(2, i - 2).c_str());
-        b = atof(c.substr(i + 1).c_str());
-      }
-    }
-    vect.push_back(make_pair(a, b));
+  Coordinate target(42.1858587, -122.6970897);  // CS building
+  vector<Coordinate> coords;
+  Coordinate coord;
+  int x;
+  char comma;
+
+  while (cin >> x >> comma >> coord.first >> comma >> coord.second) {
+    coords.push_back(coord);
   }
-  //  cout << a <<endl;
-  // cout<< b <<endl;
-
-  for (unsigned int i = 0; i < vect.size(); i++) {
-    cout << vect[i].first << " " << vect[i].second << endl;
-  }
-  // int len = vect.size();
-  // for(int i = 0; i < len; ++i){
-  //    distance = Haversine(c1, vect);}
-
-  // for (int k = 0; k< vect.size(); k++){
-  // cout << vect[k].first <<" "<< vect[k].second <<endl;
+  // for(unsigned int i = 0; i < coords.size(); i++){
+  //  cout<< coords[i].first<<coords[i].second<<endl;
   //}
 
+  SortByDistanceTo(target, coords);
+
+  cout << "\nThe closest coordinate to the SOU CS building is: " << fixed
+       << setprecision(6) << coords[0].first << ", " << coords[0].second
+       << endl;
+
   return 0;
-} 
+}
